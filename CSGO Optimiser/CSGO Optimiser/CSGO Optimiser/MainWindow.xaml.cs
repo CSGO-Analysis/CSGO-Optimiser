5561cf6cedd51d38860673d37d9f33ddfd23ec32
rfirevtre <peter@akim.dk>
2014-06-10 11:44:31 +0200

    First commit!

---
+++ CSGO Optimiser/CSGO Optimiser/CSGO Optimiser/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Controller;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSGO_Optimiser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private OptimiseController optimiseController;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            optimiseController = new OptimiseController();
+        }
+
+        private void browseButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dialog = new System.Windows.Forms.FolderBrowserDialog();
+                dialog.ShowDialog();
+                optimiseController.SetSteamPath(dialog.SelectedPath);
+                pathLabel.Content = optimiseController.GetSteamPath();
+                optimiseButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void optimiseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (optimiseController.GetSteamPath() != null)
+            {
+                int changes = 0;
+                try
+                {
+                    if (nvidiaCheckBox.IsChecked == true)
+                    {
+                        logTextBox.Text += optimiseController.SetNvidiaSettings();
+                        changes++;
+                    }
+                    if (autoexecCheckBox.IsChecked == true)
+                    {
+                        logTextBox.Text += optimiseController.CopyAutoexec();
+                        changes++;
+                    }
+                    if (ingameVideoButton.IsChecked == true)
+                    {
+                        logTextBox.Text += optimiseController.CopyVideoSettings();
+                        changes++;
+                    }
+                    if (launchOptionsCheckBox.IsChecked == true)
+                    {
+                        logTextBox.Text += optimiseController.SetLaunchOptions();
+                        changes++;
+                    }
+                    if (mouseAccCheckBox.IsChecked == true)
+                    {
+                        logTextBox.Text += optimiseController.DisableMouseAcc();
+                        changes++;
+                    }
+                    if (capsLockButton.IsChecked == true)
+                    {
+                        logTextBox.Text += optimiseController.DisableCapsLock();
+                        changes++;
+                    }
+                    if (visualThemesCheckBox.IsChecked == true)
+                    {
+                        logTextBox.Text += optimiseController.DisableVisualThemes();
+                        changes++;
+                    }
+                    logTextBox.Text += string.Format("\nOptimisation finished ({0} changes) \n", changes);
+                    logTextBox.ScrollToEnd();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please locate your Steam folder");
+            }
+        }
+    }
+}
---
+++ CSGO Optimiser/CSGO Optimiser/Controller/OptimiseController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Controller
+{
+    public class OptimiseController
+    {
+        private OptimisePaths optimisePaths;
+
+        public OptimiseController()
+        {
+            optimisePaths = new OptimisePaths();
+        }
+
+        public void SetSteamPath(string selectedPath)
+        {
+            optimisePaths.Steam = selectedPath;
+        }
+
+        public string GetSteamPath()
+        {
+            return optimisePaths.Steam;
+        }
+
+        public string GetCfgPath()
+        {
+            return optimisePaths.Cfg;
+        }
+
+        public string SetNvidiaSettings()
+        {
+            var process = Process.Start("Geforce 3D Profile Manager.exe");
+            process.WaitForExit();
+            TimeSpan time = process.ExitTime - process.StartTime;
+            return "Geforce 3D Profile Manager exited after " + time.Seconds.ToString() + " seconds. \n";
+        }
+        
+        public string CopyAutoexec()
+        {
+            string autoexec = optimisePaths.Cfg + "autoexec.cfg";
+            File.Copy("autoexec.cfg", autoexec, true);
+            return autoexec + " succesfully created. \n";
+        }
+
+        public string CopyVideoSettings()
+        {
+            string video = optimisePaths.Cfg + "video.txt";
+            File.Copy("video.txt", video, true);
+            return video + " succesfully created. \n";
+        }
+
+        public string SetLaunchOptions()
+        {
+            /*string[] dirs = Directory.GetDirectories(optimisePaths.Steam + @"\userdata\");
+            foreach (string dir in dirs)
+            {
+                if (File.Exists(dir + @"\config\localconfig.vdf"))
+                {
+                    IEnumerable<string> csgoLines = File.ReadLines(dir + @"\config\localconfig.vdf")
+                    .SkipWhile(line => !line.Equals("\t\t\t\t\t\"730\""))
+                    .TakeWhile(line => !line.Equals("\t\t\t\t\t}"));
+
+                    if (csgoLines.Contains("LaunchOptions"))
+                    {
+                        File.AppendAllText(dir + @"\config\localconfig.vdf", "test");
+                        // edit launch options
+                    }
+                    else
+                    {
+                        // add launch options on new line
+                    }
+                }
+            }*/
+            throw new NotImplementedException();
+        }
+        
+        public string DisableMouseAcc()
+        {
+            //Process.Start("EnhancedPointerPrecOff.reg");
+            System.Drawing.Size resolution = System.Windows.Forms.SystemInformation.PrimaryMonitorSize;
+            return resolution.ToString();
+        }
+
+        public string DisableCapsLock()
+        {
+            Process.Start("KillCapsLock.reg");
+            return "CapsLock succesfully disabled. \n";
+        }
+
+        public string DisableVisualThemes()
+        {
+            if (optimisePaths.CsgoExe != null)
+            {
+                var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers", true);
+                if (key == null)
+                {
+                    throw new InvalidOperationException(@"Cannot open registry key HKCU\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers.");
+                }
+                key.SetValue(optimisePaths.CsgoExe, "DISABLETHEMES");
+
+                return "DISABLETHEMES key succesfully added in " + key.Name + ". \n";
+            }
+            else
+            {
+                throw new Exception("CSGO folder was not found.");
+            }
+        }
+    }
+}
